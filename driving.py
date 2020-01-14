123cab2d74e80e98f12cd0145542deab664b61c2
JerryFL <3573764836@qq.com>
2020-01-14 19:27:38 +0800

    add 中国/美国”

--- driving.py
+++ driving.py
@@ -1,7 +1,7 @@
 country = input('请问你是哪国人：')
 input('请输入你的年龄: ')
 age = int(age)
-if country == 'China'
+if country == '中国'
     if age >= 18:
     	print('你可以考驾照')
     else:
@@ -11,3 +11,5 @@ elif country == '美国'：
         print('你可以考驾照')
     else: 
         print('你还不能考驾照') 
+else: 
+	print('你只能输入中国和美国')
